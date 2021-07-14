ededecfdea2597de3824accb4e526942bddf4451
cesar ramirez <cramirez@ifae.es>
2021-07-14 08:08:44 -0700

    preserve only one class

--- read_theory_to_xi.py
+++ read_theory_to_xi.py
@@ -19,22 +19,6 @@ def get_theory(z, source, box_path):
     ks, pdd, pdm, pmm =  np.loadtxt(box_path / f'out_pk_srcs_pop{source-1}_z{z:.3f}.txt', unpack=True)
     return ks, pdd, pdm, pmm
 
-r_values = np.array([  2.59875,   7.59625,  12.59375,  17.59125,  22.58875,  27.58625,
-        32.58375,  37.58125,  42.57875,  47.57625,  52.57375,  57.57125,
-        62.56875,  67.56625,  72.56375,  77.56125,  82.55875,  87.55625,
-        92.55375,  97.55125, 102.54875, 107.54625, 112.54375, 117.54125,
-       122.53875, 127.53625, 132.53375, 137.53125, 142.52875, 147.52625,
-       152.52375, 157.52125, 162.51875, 167.51625, 172.51375, 177.51125,
-       182.50875, 187.50625, 192.50375, 197.50125])
-
-def from_pk_to_correlation(k, pk, r_values=r_values):
-    result = []
-    for r in r_values:
-        values = k**2*pk*np.sin(k*r)/(k*r*2*np.pi**2)
-        integrand = InterpolatedUnivariateSpline(k, values, k=1)
-        result.append(integrand.integral(k[0], k[-1]))
-    return result
-
 class ReadTheoryCoLoRe:
     _default_h = 0.7
     _default_Om0 = 0.3
@@ -55,7 +39,7 @@ class ReadTheoryCoLoRe:
             zmax (float, optional): zmax to consider when dealing with nz distribution. Defaults to zmax in paramcfg if it exists, 1000 if not.
         '''
 
-        if tracer not in ('dd', 'dm', 'mm'):
+        if tracer not in ('dd', 'dm', 'mm'): # pragma: no cover
             raise ValueError('Tracer should be in "dd", "dm", "mm"')
         self.tracer = tracer
         self.box_path = Path(box_path)
@@ -66,7 +50,7 @@ class ReadTheoryCoLoRe:
         if bias_filename is None:
             try:
                 self.bias_filename = self.param_cfg[f'srcs{self.source}']['bias_filename']
-            except Exception:
+            except Exception: # pragma: no cover
                 print('Failed reading bias from param.cfg')
         else:
             self.bias_filename = bias_filename
@@ -74,7 +58,7 @@ class ReadTheoryCoLoRe:
         if nz_filename is None:
             try:
                 self.nz_filename = self.param_cfg[f'srcs{self.source}']['nz_filename']
-            except Exception:
+            except Exception: # pragma: no cover
                 print('Failed reading nz from param.cfg')
         else:
             self.nz_filename = nz_filename
@@ -104,19 +88,8 @@ class ReadTheoryCoLoRe:
 
         return param_cfg
 
-    def get_parameter_from_cfg(self, parameter):
-        print('deprecated method get_parameter_from_cfg')
-        if self.param_cfg is None:
-            if self.param_cfg_filename is None:
-                self.param_cfg_filename = next( Path(self.box_path).glob('*.cfg') )
-            
-            with open(self.param_cfg_filename) as f:
-                self.param_cfg = libconf.load(f)
-
-        return self.param_cfg[f'srcs{self.source}'][parameter]
-
     @cached_property
-    def cosmo(self):
+    def cosmo(self): # pragma: no cover
         cosmo = dict(
             omega_M = float( self.param_cfg['cosmo_par']['omega_M'] ),
             omega_L = float( self.param_cfg['cosmo_par']['omega_L'] ),
@@ -447,127 +420,8 @@ class ReadTheoryCoLoRe:
         func = InterpolatedUnivariateSpline(z, np.log(D), k=1)
         dlogDdz = func.derivative()
         return dlogDdz
-
-class ReadPkCoLoRe(ReadTheoryCoLoRe):         
-    def get_theory(self, z, bias=None):
-        ks, pdd, pdm, pmm = np.loadtxt(self.box_path / f'out_pk_srcs_pop{self.source-1}_z{z:.3f}.txt', unpack=True)
-        if self.tracer == 'dd':
-            if bias is None:
-                return ks, pdd
-            else:
-                return ks, pmm*bias**2
-        if self.tracer == 'dm':
-            if bias is None:
-                return ks, pdm
-            else:
-                return ks, pmm*bias
-        if self.tracer == 'mm':
-            return ks, pmm
-
-    @cached_property
-    def k(self):
-        return self.get_theory(0)[0]
-
-    def get_npole(self, n, rsd=True, z=None, beta=None):
-        raise ValueError('get npole not supported for pk')
-
-
-class ReadXiCoLoRe(ReadTheoryCoLoRe):
-    def get_theory(self, z, bias=None):
-        logger.debug(f'Getting theory for z: {z} and bias: {bias}')
-        ks, pdd, pdm, pmm = np.loadtxt(self.box_path / f'out_xi_srcs_pop{self.source-1}_z{z:.3f}.txt', unpack=True)
-        if self.tracer == 'dd':
-            if bias is None:
-                return ks, pdd
-            else:
-                return ks, pmm*bias**2
-        if self.tracer == 'dm':
-            if bias is None:
-                return ks, pdm
-            else:
-                return ks, pmm*bias
-        if self.tracer == 'mm':
-            return ks, pmm
-
-    @cached_property
-    def r(self):
-        return self.get_theory(0)[0]
-
-    def get_npole(self, n, z, rsd=True, bias=None):
-        '''
-        Compute the npole from the correspondend z_bin
-
-        Args:
-            n (int): multipole
-            z (float, optional): redshift value for theory.
-            bias (float, optional): force a value of bias. If not set the value of bias will be extracted from bias_file and from Pk.
-        '''
-        xi = self.get_theory(z, bias=bias)[1]
-
-        if not rsd:
-            if n == 0:
-                return xi
-            else:
-                return np.zeros_like(xi)
-        else:
-            if bias is None:
-                try:
-                    beta = self.beta_from_file(z)
-                except IndexError:
-                    logger.warning('Getting beta from CoLoRe files failed, computing growth factor...')
-                    beta = self.beta_from_growth(z)
-            else:
-                try:
-                    f = self.velocity_growth_factor(z, read_file=True)
-                except IndexError:
-                    f = self.velocity_growth_factor(z, read_file=False)
-                beta = f/bias
-
-            if np.isinf(beta) and (bias == 0):
-                xi = self.get_theory(z, bias=1)[1] # xi used should be the matter one, bias 1
-                if n == 0:
-                    return (f**2/5)*xi
-                if n == 2:
-                    xibar = self.get_xibar(xi)
-                    return (4*f**2/7.0)*(xi - xibar)
-                if n == 4:
-                    xibar = self.get_xibar(xi)
-                    xibarbar = self.get_xibarbar(xi)
-                    return 8*beta**2/35 * (xi + 5*xibar/2.0 - 7*xibarbar/2.0)
-                else:
-                    raise ValueError('n not in 0 2 4')
-
-            if n == 0:
-                return (1 + 2*beta/3.0 + beta**2/5.0)*xi
-            if n == 2:
-                xibar = self.get_xibar(xi)
-                return (4*beta/3.0 + 4*beta**2/7.0)*(xi - xibar)
-            if n == 4:
-                xibar = self.get_xibar(xi)
-                xibarbar = self.get_xibarbar(xi)
-                return 8*beta**2/35 * (xi + 5*xibar/2.0 - 7*xibarbar/2.0)
-            else:
-                raise ValueError('n not in 0 2 4')
-
-    def get_xibar(self, xi):
-        integrals = []
-        r = np.concatenate(([0],self.r)) #adding an initial value of 0, not well studied!!! WARNING!
-        xi = np.concatenate(([0], xi))
-        for i in range(1, len(r)):
-            i+=1 #To start with an array of 2 and end with an array like self.r
-            integrals.append( simple_integral(r[:i],  xi[:i]*r[:i]**2))
-        return 3*np.array(integrals)/self.r**3
-
-    def get_xibarbar(self, xi):
-        integrals = []
-        r = np.concatenate(([0], self.r))
-        xi = np.concatenate(([0], xi))
-        for i in range(1, len(r)):
-            i += 1 
-            integrals.append( simple_integral(r[:i], xi[:i]*r[:i]**4))
-        return 5*np.array(integrals)/self.r**5
-        
-class ReadXiCoLoReFromPk(ReadXiCoLoRe):
+       
+class ReadXiCoLoReFromPk(ReadTheoryCoLoRe):
     def __init__(self, box_path, source, tracer='dd', bias_filename=None, nz_filename=None, pk_filename=None, param_cfg_filename=None, zmin=None, zmax=None, smooth_factor=None, apply_lognormal=True):
         super().__init__(box_path=box_path,
                         source=source,
@@ -766,104 +620,6 @@ class ReadXiCoLoReFromPk(ReadXiCoLoRe):
                                    Ob0=self.cosmo['omega_B'])
         r_max = cosmo.comoving_distance(float( self.param_cfg['global']['z_max'] ))*self.cosmo['h']
         return 2*r_max.value*(1+2/self.param_cfg['field_par']['n_grid']) 
-
-class CAMBCorrelation(ReadXiCoLoRe):
-    def __init__(self, box_path, source, bias_filename=None, nz_filename=None, param_cfg_filename=None, output_save_location=None, zmin=None, zmax=None):
-        logger.info(f'Initializing CAMBCorrelation instance for box {box_path}')
-        super().__init__(box_path=box_path,
-                        source=source,
-                        tracer='dd',
-                        bias_filename=bias_filename,
-                        nz_filename=nz_filename,
-                        param_cfg_filename=param_cfg_filename,
-                        zmin=zmin,
-                        zmax=zmax)
-
-        self.output_save_location = output_save_location
-        
-        if logging.root.level <= logging.DEBUG:
-            logger.debug('Cosmological parameters:')
-            for key in self.cosmo.keys():
-                logger.debug(f'\t{key}\t{self.cosmo[key]}')
-
-        h = self.cosmo['h']
-        self.pars = camb.CAMBparams()
-        if False:
-            self.pars.set_cosmology(H0=67.31, ombh2=0.022, omch2=0.122)
-            self.pars.InitPower.set_params(ns=0.965)
-        else:
-            self.pars.set_cosmology(H0=h*100, 
-                ombh2=self.cosmo['omega_B']*h**2, 
-                omch2=(self.cosmo['omega_M'] - self.cosmo['omega_B'])*h**2,
-                # omk=self.cosmo['omega_K'],
-                # TCMB=self.cosmo['T_CMB'],
-                )
-            self.pars.InitPower.set_params(ns=self.cosmo['ns'])
-
-            # default_AS = self.pars.InitPower.ScalarPowerAmp[0]
-            # self.pars.set_matter_power(redshifts=[0.0], kmax=100.0)
-            # default_results = camb.get_results(self.pars)
-            # default_sigma_8 = default_results.get_sigma8()[0]
-            # new_As = default_AS*((sigma_8/default_sigma_8)**2)
-            # self.pars.InitPower.set_params(As=new_As)
-
-    @cached_property
-    def r(self):
-        return super().get_theory(0)[0]
-
-    def get_theory(self, z, bias=None):
-        if self.output_save_location is None:
-            _, xi = self.get_theory_CAMB(z)
-        else:
-            try:
-                if bias is None:
-                    xi = np.load( Path(self.output_save_location / f'{z:.3f}.npy') )
-                else:
-                    xi = np.load( Path(self.output_save_location / f'{z:.3f}_{bias:.3f}.npy'))
-                logger.info(f'Obtaining theoretical values from file. z = {z}')
-            except FileNotFoundError:
-                logger.info(f'File not found, computing new theoretical values. z = {z}. b = {bias}')
-                _, xi = self.get_theory_CAMB(z, bias=bias)
-                np.save( Path(self.output_save_location / f'{z:.3f}.npy'), xi)
-
-        return np.array((self.r, xi))
-
-    def get_theory_CAMB(self, z, return_pk=False, bias=None):
-        '''
-            Get theory predicted by CAMB (correlation)
-
-            Args:
-                z (double): Redshift of the prediction
-                return_pk (bool, optional): Return power spectrum. Default: False
-        '''
-        logger.debug('Setting matter power spectrum')
-        self.pars.set_matter_power(redshifts=[z], kmax=50)
-
-        #Linear spectra
-        logger.debug('Getting results from parameters')
-        self.pars.NonLinear = model.NonLinear_none
-        self.results = camb.get_results(self.pars)
-        
-        logger.debug(f'Getting matter power spectrum for redshift {z}')
-        kh, z, pk = self.results.get_matter_power_spectrum(minkh=1e-4, maxkh=10, npoints = 9999)
-
-        if bias is None:
-            bias = self.bias(z)
-        pk *= bias**2
-
-        logger.debug('Converting power spectrum into correlation')
-        if return_pk:
-            return np.array((kh, pk[0,:]))
-        else:
-            return np.array((self.r, from_pk_to_correlation(kh, pk[0,:], self.r)))
-
-    def clean_results(self):
-        if self.output_save_location is None:
-            raise ValueError('No output save location defined')
-
-        for file in self.output_save_location.glob('*.npy'):
-            file.unlink(missing_ok=True)
-
 
 def simple_integral(x, y):
     integrand = InterpolatedUnivariateSpline(x, y, k=1)
